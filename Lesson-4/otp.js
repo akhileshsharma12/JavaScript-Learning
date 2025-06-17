2427b1301d39873cf5771f0da740a5da1a9cc52a
akhileshsharma12 <akhileshsh421@gmail.com>
2025-06-17 18:22:23 +0530

    otp generator

---
+++ Lesson-4/otp.js
@@ -0,0 +1,14 @@
+
+let yourOtp = function () {
+    let digits = '0123456789';
+    let otp = '';
+    let otpLength = 6;
+
+    for(let i = 0; i < otpLength; i ++) {
+        otp += digits[Math.floor(Math.random() * 10) ]
+    }
+
+    return otp;
+}
+
+console.log("Your OTP is: " + yourOtp());
