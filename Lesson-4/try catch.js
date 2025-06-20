3a927b09cfb45dcee1b581d064219a582dea9331
akhileshsharma12 <akhileshsh421@gmail.com>
2025-06-20 23:43:30 +0530

    try catch learning

---
+++ Lesson-4/try catch.js
@@ -0,0 +1,10 @@
+let isQualified = true;
+
+try {
+    if(isQualified) {
+        console.log('Congratulations! You are qualified.');
+    }
+}
+catch(error) {
+    console.log('Error :', error);
+}
